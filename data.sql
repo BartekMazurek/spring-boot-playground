e61554881b56ea09a27c5d9ae6268b246b1cb98b
bartek <bartlomiejmazurek9@gmail.com>
2023-09-22 19:45:10 +0200

    Fixed SQL script

--- data.sql
+++ data.sql
@@ -1,9 +1,11 @@
 create table users
 (
-    id      	INT PRIMARY KEY SERIAL,
+    id      	INT PRIMARY KEY AUTO_INCREMENT,
     firstname   VARCHAR(20),
-    lastname	VARCHAR(25),
+    lastname	VARCHAR(25)
 );
+
 
 insert into users (firstname, lastname) values ("John", "Doe");
 insert into users (firstname, lastname) values ("Jane", "Doe");
+insert into users (firstname, lastname) values ("Thomas", "Doe");
